c28f28cf544bd467e8131a309f69d4bb0ef61d49
Brian Cherinka <havok2063@hotmail.com>
2018-07-05 10:37:08 -0400

    adding the telescope constants from instrument object into new telescope object

--- specsim/telescope.py
+++ specsim/telescope.py
@@ -6,7 +6,7 @@
 # @Author: Brian Cherinka
 # @Date:   2018-06-20 14:45:48
 # @Last modified by:   Brian Cherinka
-# @Last Modified time: 2018-06-20 16:50:33
+# @Last Modified time: 2018-07-05 10:36:39
 
 from __future__ import print_function, division, absolute_import
 
@@ -14,8 +14,11 @@ from __future__ import print_function, division, absolute_import
 class Telescope(object):
     ''' A Telescope configuration '''
 
-    def __init__(self, name):
+    def __init__(self, name, **kwargs):
         self.name = name
+
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
     def __repr__(self):
         return '<Telescope(name={0})>'.format(self.name)
@@ -29,9 +32,10 @@ def initialize(config, params=None):
     size = params.get('telescope', '') if params else ''
     name = '{0} {1}'.format(survey, size)
 
+    # copy the from telescope constants over from the Instrument object
     area_params = config.get_constants(config.instrument)
 
-    telescope = Telescope(name)
+    telescope = Telescope(name, **area_params)
     return telescope
 
 
